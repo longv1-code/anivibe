547198a6b4849cca2b2c1b7dea6dd013be92e27c
Long Vo <longvo.work84@gmail.com>
2026-05-12 18:51:56 -0500

    fixed genre field, added order_by and sort field

--- backend/services/jikan.py
+++ backend/services/jikan.py
@@ -6,20 +6,69 @@ import httpx
 
 JIKAN_BASE_URL = "https://api.jikan.moe/v4"
 
-async def search_anime(query: str, genre: str = None, min_score: float = None, type: str = None, status: str = None, min_episodes: int = None):
-    params = {"q": query, "limit": 20}
+GENRE_MAP = {
+    "Action" : 1,
+    "Adventure" : 2,
+    "Cars" : 3,
+    "Comedy" : 4, 
+    "Avante Garde" : 5,
+    "Demons" : 6,
+    "Mystery" : 7,
+    "Drama" : 8,
+    "Ecchi" : 9,
+    "Fantasy" : 10,
+    "Game" : 11,
+    "Hentai" : 12,
+    "Historical" : 13,
+    "Horror" : 14,
+    "Kids" : 15,
+    "Martial Arts" : 17,
+    "Mecha" : 18,
+    "Music" : 19,
+    "Parody" : 20,
+    "Samurai" : 21,
+    "Romance" : 22,
+    "School" : 23,
+    "Sci Fi" : 24,
+    "Shoujo" : 25,
+    "Girls Love" : 26,
+    "Shounen" : 27,
+    "Boys Love" : 28,
+    "Space" : 29,
+    "Sports" : 30,
+    "Super Power" : 31, 
+    "Vampire" : 32,
+    "Harem" : 35,
+    "Slice Of Life" : 36,
+    "Supernatural" : 37, 
+    "Military" : 38,
+    "Police" : 39,
+    "Psychological" : 40,
+    "Suspense" : 41,
+    "Seinen" : 42,
+    "Josei" : 43,
+    "Award Winning" : 46,
+    "Gourmet" : 47,
+    "Work Life" : 48,
+    "Erotica" : 49,
+}
+
+async def search_anime(query: str, genres: str = None, min_score: float = None, type: str = None, status: str = None, min_episodes: int = None, order_by: str = "popularity", sort: str = "desc"):
+    params = {"q": query, "limit": 20, "min_score": min_score, "order_by": order_by, "sort": sort}
 
     # filters
-    if genre:
-        params["genre"] = genre
-    if min_score:
-        params["min_score"] = min_score
+    if genres:
+        genre_id = GENRE_MAP.get(genres.title(), None)
+        if genre_id:
+            params["genres"] = genre_id
     if type:
         params["type"] = type
     if status:
         params["status"] = status
     if min_episodes:
         params["min_episodes"] = min_episodes
+    if min_score:
+        params["min_score"] = min_score
 
     try:
         async with httpx.AsyncClient() as client:
@@ -27,7 +76,7 @@ async def search_anime(query: str, genre: str = None, min_score: float = None, t
             response.raise_for_status()
             data = response.json()
 
-            # data for each anime
+            # data for each anime as Python dict
             results = [
                 {
                     "title": anime["title"],
