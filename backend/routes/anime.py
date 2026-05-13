5772da59e4b30da29ef3c70c1842454b4d4836ed
Long Vo <longvo.work84@gmail.com>
2026-05-12 20:09:26 -0500

    added test prints

--- backend/routes/anime.py
+++ backend/routes/anime.py
@@ -16,8 +16,12 @@ router = APIRouter() # creates APIRouter instance
 @router.post("/anime/search") # POST to send data to server, cleaner URLs, semantically correct 
 async def search_anime(request: SearchRequest):
     try:
+        print("USER QUERY:", request.prompt)
         params = await gemini.extract_search_params(request.prompt) # calls openai function with prompt
-        results = await jikan.search_anime(**params) # ** operator unpacks Python dict into keyword args
-        return results
+        print("GEMINI PARAMS:", params)
+        results = await jikan.search_anime(**params) # ** operator unpacks Python dict into keyword args]
+        print(results)
+        return {"results" : results, "params" : params}
     except Exception as e:
+        print("ERROR:", e)
         raise HTTPException(status_code=500, detail=str(e))
