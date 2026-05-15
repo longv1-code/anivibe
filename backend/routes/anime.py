f7deee26223c3058dcf2aa3b174507384bd0a67d
Long Vo <longvo.work84@gmail.com>
2026-05-15 16:52:03 -0500

    post req for FilterRequest

--- backend/routes/anime.py
+++ backend/routes/anime.py
@@ -11,17 +11,29 @@ from services import gemini
 class SearchRequest(BaseModel):
     prompt: str
 
+class FilterRequest(BaseModel):
+    filters: dict
+
 router = APIRouter() # creates APIRouter instance
 
 @router.post("/anime/search") # POST to send data to server, cleaner URLs, semantically correct 
 async def search_anime(request: SearchRequest):
     try:
         print("USER QUERY:", request.prompt)
-        params = await gemini.extract_search_params(request.prompt) # calls openai function with prompt
+        params = await gemini.extract_search_params(request.prompt) # calls openai function with prompt, return Python dict
         print("GEMINI PARAMS:", params)
         results = await jikan.search_anime(**params) # ** operator unpacks Python dict into keyword args]
         print(results)
         return {"results" : results, "params" : params}
+    except Exception as e:
+        print("ERROR:", e)
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.post("/anime/filter")
+async def filter_anime(request: FilterRequest):
+    try:
+        results = await jikan.search_anime(**request.filters)
+        return {"results" : results, "params" : request.filters}
     except Exception as e:
         print("ERROR:", e)
         raise HTTPException(status_code=500, detail=str(e))
