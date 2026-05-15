efb4b42a5dc29be8aa6aa71ccb9e8740192a032c
Long Vo <longvo.work84@gmail.com>
2026-05-15 16:51:03 -0500

    added filter functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import SearchBar from './components/Searchbar'
 import axios from 'axios'
 import { useState } from 'react'
 import AnimeGrid from './components/AnimeGrid'
+import FilterPanel from './components/FilterPanel'
 
 const App = () => {
   // states for re-rendering
@@ -14,17 +15,39 @@ const App = () => {
     try {
       setIsLoading(true)
       console.log("user searched for:", query)
+      setSearchQuery(query)
       const response = await axios.post("http://localhost:8000/anime/search", { // call POST request on /anime/search to run function search_anime in anime.py
         prompt: query
       })
-      setResults(response.data) // response is axios object, .data has the array of anime
+      setResults(response.data.results) // response is axios object, .data has the array of anime
+      setFilters(response.data.params)
       setIsLoading(false)
     } catch (e) { // catch error e and display err msg
-      console.log("Search failed: ", e)
+      console.log("Search failed:", e)
       setIsLoading(false)
     }
 
   }
+
+  const handleFilterChange = (key, value) => {
+    setFilters(prev => ({...prev, [key]: value})) // prev contain original state before change, something React does internally with an arrow function inside setter function
+  }
+
+  const handleApplyFilters = async () => { // function for Apply Filters button
+    try {
+      setIsLoading(true)
+      const response = await axios.post("http://localhost:8000/anime/filter", {
+        filters: filters
+      })
+      setResults(response.data.results)
+      setFilters(response.data.params)
+      setIsLoading(false)
+    } catch (e) {
+      console.log("Filter failed:", e)
+      setIsLoading(false)
+    }
+  }
+
   // SearchBar function shows the search bar feature
   // loading message is only true if isLoading, clever use of &&
   // results only show if there is results (uses length to check rather if object exists) and show grid of anime
@@ -33,7 +56,8 @@ const App = () => {
     <div>
       <SearchBar onSearch={handleSearch} /> 
       {isLoading && <p>Loading...</p>} 
-      {results.length > 0 && <AnimeGrid results={results} />}
+      {results.length > 0 ? (<AnimeGrid results={results} />) : (<p>No anime can be found, try a different prompt or filter!</p>)}
+      {Object.keys(filters).length > 0 && <FilterPanel filters={filters} onFilterChange={handleFilterChange} onApplyFilters={handleApplyFilters}/>}
     </div>
   )
 }
